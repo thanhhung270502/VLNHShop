c3770d1107f9ad051f8dac9734262c923cf44608
locdacknownothing <98312612+locdacknownothing@users.noreply.github.com>
2022-12-07 10:51:12 +0700

    add database

---
+++ models/product.php
@@ -0,0 +1,27 @@
+<?php
+    include_once('connection.php');
+    $result = $conn->query("SELECT * FROM product");
+    // $products = [];
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($product = $result->fetch_assoc()) {
+            echo 'id: '.$product['id'];
+            echo 'name: '.$product['name'];
+            echo 'price: '.$product['price'];
+            echo 'color: '.$product['color'];
+            echo 'img: '.$product['img'];
+            echo 'quantity_remain: '.$product['quantity_remain'];
+        }
+    }
+    // foreach ($req->fetch_all(MYSQLI_ASSOC) as $product) {
+    //     $products[] = new Product(
+    //         echo 'id: '.$product['id'];
+    //         echo 'name: '.$product['name'];
+    //         echo 'price: '.$product['price'];
+    //         echo 'color: '.$product['color'];
+    //         echo 'img: '.$product['img'];
+    //         echo 'quantity_remain: '.$product['quantity_remain'];
+    //     );
+    // }
+?>
