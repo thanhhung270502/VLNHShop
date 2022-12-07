102337ddb2d1e490641c8d73e274df460ffd9139
Nguyen Truong <binclct@gmail.com>
2022-12-07 11:11:46 +0700

    Nearly complete home page

---
+++ app/views/footer.php
@@ -0,0 +1,68 @@
+<!-- Footer -->
+<footer class="bg-dark mt-10">
+    <!-- Menus & NewsLetter -->
+    <div class="border border-dark border-top">
+        <div class="container">
+            <div class="row my-4 text-white">
+
+                <!-- Footer Nav-->
+                <nav class="col-3 col-md mb-4 mb-md-0">
+                    <h6 class="mb-4 fw-bolder fs-6">Company</h6>
+                    <ul class="list-unstyled">
+                        <li class="mb-2"><a class="text-decoration-none text-white opacity-75 opacity-25-hover transition-all" href="#">About Us</a></li>
+                        <li class="mb-2"><a class="text-decoration-none text-white opacity-75 opacity-25-hover transition-all" href="#">Our Blog</a></li>
+                        <li class="mb-2"><a class="text-decoration-none text-white opacity-75 opacity-25-hover transition-all" href="#">FAQs</a></li>
+                        <li class="mb-2"><a class="text-decoration-none text-white opacity-75 opacity-25-hover transition-all" href="#">Contact</a></li>
+                    </ul>
+                </nav>
+                <!-- /Footer Nav-->
+
+                <!-- Footer Nav-->
+                <nav class="col-3 col-md mb-4 mb-md-0">
+                    <h6 class="mb-4 fw-bolder fs-6">Navigation</h6>
+                    <ul class="list-unstyled">
+                        <li class="mb-2"><a class="text-decoration-none text-white opacity-75 opacity-25-hover transition-all" href="#">Register</a></li>
+                        <li class="mb-2"><a class="text-decoration-none text-white opacity-75 opacity-25-hover transition-all" href="#">Cart</a></li>
+                        <li class="mb-2"><a class="text-decoration-none text-white opacity-75 opacity-25-hover transition-all" href="#">Checkout</a></li>
+                        <li class="mb-2"><a class="text-decoration-none text-white opacity-75 opacity-25-hover transition-all" href="#">Account</a></li>
+                    </ul>
+                </nav>
+                <!-- /Footer Nav-->
+
+                <!-- Footer Contact-->
+                <div class="col-6">
+                    <h6 class="mb-4 fw-bolder fs-6">Join Our Newsletter</h6>
+                    <p class="opacity-75">Sign up to our newsletter and we'll email you a code worth 15%
+                        off your first order. By subscribing to our mailing list you agree to our terms and conditions.</p>
+                    <form class="bg-white d-flex justify-content-start align-items-center border-dark-focus-within transition-all mt-4">
+                        <div class="input-group m-0">
+                            <input type="text" class="form-control d-flex flex-grow-1 border-0 bg-transparent py-3" placeholder="Enter your email" aria-label="Enter your email">
+                            <span class="input-group-text bg-transparent border-0"><i class="ri-arrow-right-line align-middle"></i></span>
+                        </div>
+                    </form>
+                </div>
+                <!-- /Footer Contact-->
+
+
+            </div>
+            <div class="row border-light border-top">
+                <div class="col-6">
+                    <p class="small opacity-75 text-white mt-3">&copy; 2021 Alpine All Rights Reserved. Template by <a class="text-white" href="https://www.pixelrocket.store">Pixel Rocket</a></p>
+                </div>
+                <div class="col-6">
+                    <div class="container d-flex justify-content-end align-items-center mt-3    ">
+                        <p class="lead fw-bolder mb-0 lh-1 text-white">Find us online</p>
+                        <ul class="list-unstyled d-flex justify-content-start align-items-center mb-0 ms-3 lh-1">
+                            <li class="mx-1 mb-0 lh-1"><a class="text-muted text-decoration-none opacity-75-hover transition-all lh-1" href="#"><i class="ri-instagram-fill ri-xl lh-1"></i></a></li>
+                            <li class="mx-1 mb-0 lh-1"><a class="text-muted text-decoration-none opacity-75-hover transition-all lh-1" href="#"><i class="ri-facebook-fill ri-xl lh-1"></i></a></li>
+                            <li class="mx-1 mb-0 lh-1"><a class="text-muted text-decoration-none opacity-75-hover transition-all lh-1" href="#"><i class="ri-twitter-fill ri-xl lh-1"></i></a></li>
+                            <li class="mx-1 mb-0 lh-1"><a class="text-muted text-decoration-none opacity-75-hover transition-all lh-1" href="#"><i class="ri-snapchat-fill ri-xl lh-1"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /Menus & NewsLetter -->
+</footer>
+<!-- /Footer -->
