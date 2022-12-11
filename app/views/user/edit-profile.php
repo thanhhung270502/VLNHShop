8d866f7d22721ab1877b5bb6084d3da2a6d8d0ad
VuTran2601 <nguyenvutran2601@gmail.com>
2022-12-11 19:19:48 +0700

    Update edit-profile.php

--- app/views/user/edit-profile.php
+++ app/views/user/edit-profile.php
@@ -37,7 +37,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Edit User</title>
+        <title>My Profile</title>
 
         <link
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
