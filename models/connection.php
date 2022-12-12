8d6cdeacdd167a3edb43a7c4b9b7cabdb36e474f
VuTran2601 <nguyenvutran2601@gmail.com>
2022-12-12 07:45:57 +0700

    Update connection.php

--- models/connection.php
+++ models/connection.php
@@ -4,11 +4,10 @@ $username = "root";
 $password = "";
 $database = "shop";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database, 3307);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// echo "Connection sucess";
-?>
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $database, 3307);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    
+    // echo "Connection sucess";
